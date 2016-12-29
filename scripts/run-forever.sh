c86b33a5118400e9b45e2e8dcdff8c8f6b9efc31
Dr. Thomas Franz <thomas.franz@adesso.de>
2016-12-29 12:36:57 +0100

    Forever-Script changed to support local-only installation

--- scripts/run-forever.sh
+++ scripts/run-forever.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-forever start -l forever.log -o out.log -e err.log ../node_modules/http-server/bin/http-server ../dist
+../node_modules/forever/bin/forever start -l forever.log -o out.log -e err.log ../node_modules/http-server/bin/http-server ../dist
