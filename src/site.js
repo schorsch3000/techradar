7d14ccb6f00588b564ef390448a6f8f35df2ac51
Dr. Thomas Franz <thomas.franz@adesso.de>
2017-01-30 13:57:52 +0100

    - made radar data a parameter for radar-creation - switched to semicolon-separated data-format to simplify radar-data manipulation with excel and excel csv exports

--- src/site.js
+++ src/site.js
@@ -6,5 +6,9 @@ require('./images/radar_legend.png');
 
 
 const createRadarFromCsv = require('./util/factory');
+/**
+ * Insert your radar file below or substitute the placeholder radar.csv file with your radar.csv file.
+ */
+const rawCsv = require("raw-loader!../radars/radar.csv");
 
-createRadarFromCsv();
+createRadarFromCsv(rawCsv);
